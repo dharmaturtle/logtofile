687f957f6cd03c5b3d2bcab6581103a613980e1c
Alex <dharmaturtle@gmail.com>
2020-04-04 09:07:14 -0500

    Added a failing test in Visual Studio 16.6.0 Preview 2.1

--- FSharpTests/Tests.fs
+++ FSharpTests/Tests.fs
@@ -1,8 +1,12 @@
 module Tests
 
+open FSharp.Text.RegexProvider
 open System
 open Xunit
 
+type BraceRegex = Regex< ".*" >
+
 [<Fact>]
 let ``My test`` () =
+    let asdf = BraceRegex()
     Assert.True(true)
