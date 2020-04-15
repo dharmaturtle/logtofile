bdabc46decd86f9148b2d5c03354999c42e9efd0
Alex <dharmaturtle@gmail.com>
2020-04-15 13:34:24 -0500

    Attempt 1

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,100 +1,56 @@
-using Sandbox;
 using System;
-using System.Collections;
-using System.Linq;
 using System.Collections.Generic;
-using Xunit;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Tests {
+class Solution {
+  static void Main(String[] args) {
+    string inputData = "";
+    string line;
+    while ((line = Console.ReadLine()) != null) {
+      inputData += line + "\n";
+    }
+    // Do not edit: Output solution to console
+    Console.WriteLine(codeHere(inputData));
+  }
 
-  public class Solution {
-    public static void DoThing(string inputData) {
-      string line;
-      //while ((line = Console.ReadLine()) != null) {
-      //  inputData += line + "\n";
-      //}
-      // Do not edit: Output solution to console
-      Console.WriteLine(codeHere(inputData));
+  public static String codeHere(String inputData) {
+    var lines = inputData.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+    var sentence = lines[0];
+    var firstWord = lines[1];
+    var secondWord = lines[2];
+
+    // remove punctuation from words
+    var onlyLetters = new Regex(@".*?(\w+).*?", RegexOptions.Compiled);
+    var words = sentence.Split(' ').Select(word => onlyLetters.Match(word).Groups[1].Value).ToList();
+
+    // check for bad input
+    if (String.IsNullOrEmpty(sentence) || String.IsNullOrEmpty(firstWord) || string.IsNullOrEmpty(secondWord)) {
+      return (-1).ToString();
     }
 
-    static String codeHere(String inputData) {
-      var lines = inputData.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+    var indexedWords = words.Select((word, i) => (word, i));
+    var firstWords = indexedWords.Where(x => x.word == firstWord).ToList();
+    var secondWords = indexedWords.Where(x => x.word == secondWord).ToList();
 
-      var nums = lines[1].Split(' ').Select(int.Parse).ToList();
-      var countByX = new Dictionary<int, int>();
-      foreach (var x in nums) {
-        if (countByX.ContainsKey(x)) {
-          countByX[x]++;
-        } else {
-          countByX.Add(x, 1);
+    // cartesian product between first and second words. Add to list positive index differences
+    var diffs = new List<int>();
+    for (int i = 0; i < firstWords.Count(); i++) {
+      for (int k = 0; k < secondWords.Count(); k++) {
+        var a = firstWords[i].;
+        var b = secondWords.[k];
+        var diff = b - a;
+        if (diff > 0) {
+          diffs.Add(diff);
         }
       }
-      var max = countByX.Max(x => x.Value);
-      var maxOptions = countByX.Where(kvp => kvp.Value == max);
-      var min = maxOptions.Select(maxOption => {
-        var key = maxOption.Key;
-        var first = nums.IndexOf(key);
-        var last = nums.LastIndexOf(key);
-        return last - first + 1;
-      }).Min();
-
-      // Use this function to return your solution.
-      return min.ToString();
-    }
-  }
-
-  public class UnitTest1 {
-    [Fact]
-    public void T1() {
-      //"Hello world".D();
-      Solution.DoThing(@"5
-1 2 2 3 1");
     }
 
-    [Theory]
-    [InlineData("a", "b")]
-    public void T2(params string[] inputs) {
-      foreach (var input in inputs) {
-        input.D();
-      }
-    }
-
-    [Theory]
-    [InlineData(new object[] { new int[] { 1, 2 }, "" })]
-    public void T3(int[] inputs, string i) {
-      foreach (var input in inputs) {
-        input.D();
-      }
-      i.D();
-    }
-
-    [Theory]
-    [InlineData(1, 2, 3)]
-    [InlineData(-4, -6, -10)]
-    [InlineData(-2, 2, 0)]
-    [InlineData(int.MinValue, -1, int.MaxValue)]
-    public void T4(int i1, int i2, int i3) {
-      i1.D();
-      i2.D();
-      i3.D();
-    }
-
-    public class T5Data: IEnumerable<object[]> {
-      public IEnumerator<object[]> GetEnumerator() {
-        yield return new object[] { 1, 2, 3 };
-        yield return new object[] { -4, -6, -10 };
-        yield return new object[] { -2, 2, 0 };
-        yield return new object[] { int.MinValue, -1, int.MaxValue };
-      }
-      IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    }
-    [Theory]
-    [ClassData(typeof(T5Data))]
-    public void T5(int value1, int value2, int expected) {
-      value1.D();
-      value2.D();
-      expected.D();
-    }
-
+    return
+        (diffs.Count > 0m
+        ? diffs.Min()
+        : -1
+        ).ToString();
   }
 }
+
